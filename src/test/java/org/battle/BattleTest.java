8f87e4a309414e2d82fc17104b11b44cef38a77f
churchhellah <90222741+churchhellah@users.noreply.github.com>
2024-09-26 23:09:13 +0300

    Играюсь с @ParameterizedTest и @CsvSource

--- src/test/java/org/battle/BattleTest.java
+++ src/test/java/org/battle/BattleTest.java
@@ -4,8 +4,27 @@ import org.battle.models.Character;
 import org.battle.models.Weapon;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BattleTest {
+    Weapon stick = new Weapon("STICK", 1);
+    Weapon gun = new Weapon("GUN", 1000);
+    Character dipsy = new Character("ЗЕЛЁНЫЙ", "ДИПСИ", 10, stick, 5);
+    Character tinkyWinky = new Character("ФИОЛЕТОВЫЙ", "ТИНКИ ВИНКИ", 10, stick, 5);
+    Character lalla = new Character("ЖЁЛТЫЙ", "ЛЯЛЯ", 10, gun, 5);
+    Character po = new Character("КРАСНЫЙ", "ПО", 10, gun, 5);
+    Team team1 = new Team("ТЕЛЕПУЗИКИ1", dipsy, tinkyWinky);
+    Team team2 = new Team("ТЕЛЕПУЗИКИ2", lalla, po);
+    Battle battle = new Battle(team1, team2);
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "dipsy, tinkyWinky, lalla, po"
+    })
+    void testBattleStart(Character... teletubbies) {
+        Battle battle = new Battle()
+    }
 
     @Test
     void testBattleStart() {
@@ -23,6 +42,7 @@ class BattleTest {
         Assertions.assertFalse(team1.hasAliveMembers());
         Assertions.assertTrue(team2.hasAliveMembers());
     }
+
     @Test
     void testBattleStartsWithNoException() {
         Weapon stick = new Weapon("STICK", 1);
