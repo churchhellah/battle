a9f629c92fac54fd0edf9155a585f88e5d5b68c5
churchhellah <90222741+churchhellah@users.noreply.github.com>
2024-10-01 23:20:04 +0300

    Создаю Generic Stack

--- src/main/java/org/battle/models/AbstractWeapon.java
+++ src/main/java/org/battle/models/AbstractWeapon.java
@@ -1,6 +1,7 @@
 package org.battle.models;
 
 import lombok.AllArgsConstructor;
+import org.battle.exceptions.StackFullException;
 
 @AllArgsConstructor
 public abstract class AbstractWeapon {
@@ -14,6 +15,31 @@ public abstract class AbstractWeapon {
     private boolean isEmptyClip;
 
     // Метод создания патрона
+    private Ammo createAmmo(String ammoType) {
+        switch (ammoType) {
+            case "SELF_MADE":
+                return Ammo.SELF_MADE;
+            case "ARMOR_PIERSING":
+                return Ammo.ARMOR_PIERSING;
+            case "DEFAULT":
+                return Ammo.DEFAULT;
+            default:
+                return Ammo.DEFAULT;
+        }
+    }
+
     // Метод перезарядки
+    public void reload(String ammoType) {
+        for (int i = 0; i < maxClipSize; i++) {
+            try {
+                clip.push(createAmmo(ammoType));
+            } catch (StackFullException e) {
+
+            }
+        }
+    }
     // Метод получения патрона для выстрела
+    public Ammo getAmmoForShoot() {
+        return clip.getAmmo();
+    }
 }
